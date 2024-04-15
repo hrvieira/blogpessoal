1ecfdfa53668b19cb6286f18c56f0bfda33a31e6
hrvieira <luizhrvieira@outlook.com>
2024-04-15 12:27:00 -0300

    Blog Pessoal - Listar Postagens

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Postagem } from "../entities/postagem.entity";
+import { PostagemService } from "../services/postagem.service";
+
+@Controller("/postagens")
+export class PostagemController {
+     constructor(private readonly postagemService: PostagemService) { }
+
+     @Get()
+     @HttpCode(HttpStatus.OK)
+     findAll(): Promise<Postagem[]> {
+          return this.postagemService.findAll();
+     }
+
+}
